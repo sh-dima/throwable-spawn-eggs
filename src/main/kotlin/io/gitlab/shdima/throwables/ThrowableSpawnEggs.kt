425cf0a8a1b02e19e3e1020ddc66e49362b2efb2
Дима Ш. <dima.o.sh@proton.me>
2025-10-28 21:00:56 +0000

    Spawn the mob once the egg lands
    Co-authored-by: rolyPolyVole <yezhanting@gmail.com>

--- src/main/kotlin/io/gitlab/shdima/throwables/ThrowableSpawnEggs.kt
+++ src/main/kotlin/io/gitlab/shdima/throwables/ThrowableSpawnEggs.kt
@@ -1,15 +1,23 @@
 package io.gitlab.shdima.throwables
 
 import org.bstats.bukkit.Metrics
+import org.bukkit.NamespacedKey
+import org.bukkit.entity.EntityType
 import org.bukkit.entity.ThrownExpBottle
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.CreatureSpawnEvent
+import org.bukkit.event.entity.ProjectileHitEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.SpawnEggMeta
+import org.bukkit.persistence.PersistentDataType
 import org.bukkit.plugin.java.JavaPlugin
 
 @Suppress("unused")
 class ThrowableSpawnEggs : JavaPlugin(), Listener {
+
+    private val key = NamespacedKey(this, "throwable")
 
     override fun onEnable() {
         server.pluginManager.registerEvents(this, this)
@@ -30,7 +38,32 @@ class ThrowableSpawnEggs : JavaPlugin(), Listener {
         if (!type.toString().endsWith("_SPAWN_EGG")) return
 
         event.player.launchProjectile<ThrownExpBottle>(ThrownExpBottle::class.java, event.player.location.direction) {
-            it.item = ItemStack(type)
+            it.item = ItemStack(type).apply {
+                editMeta(SpawnEggMeta::class.java) {
+                    it.persistentDataContainer[key, PersistentDataType.BOOLEAN] = true
+                }
+            }
         }
+    }
+
+    @EventHandler
+    private fun onEggLand(event: ProjectileHitEvent) {
+        val entity = event.entity as? ThrownExpBottle ?: return
+
+        val item = entity.item
+        val meta = item.itemMeta as? SpawnEggMeta ?: return
+        val data = meta.persistentDataContainer
+        val isThrowable = data[key, PersistentDataType.BOOLEAN] ?: return
+        if (!isThrowable) return
+
+        val type = EntityType.entries.firstOrNull {
+            item.type.name == "${it.name}_SPAWN_EGG"
+        } ?: return
+
+        entity.world.spawnEntity(
+            entity.location,
+            type,
+            CreatureSpawnEvent.SpawnReason.SPAWNER_EGG
+        )
     }
 }

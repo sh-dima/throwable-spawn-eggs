368db304c9d385077d790de60512b51e6659a574
Дима Ш. <dima.o.sh@proton.me>
2026-01-11 22:18:54 +0000

    Update nixpkgs channel

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 	description = "A Minecraft Paper plugin that makes every spawn egg throwable for a more fun way to spawn mobs";
 
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 		flake-utils.url = "github:numtide/flake-utils";
 	};
 
